a952bdef54536f5116cbdc7dd6c15a1f055163aa
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2021-09-29 21:13:23 +0300

    lesson 2 commented error

--- src/main/java/com/javaguru/studet_vitalijs_usakovs/HelloWorld.java
+++ src/main/java/com/javaguru/studet_vitalijs_usakovs/HelloWorld.java
@@ -1,8 +1,8 @@
 package com.javaguru.studet_vitalijs_usakovs;
 
 public class HelloWorld {
-}    public static void main(String[] args) {
-    System.out.println("Hello World");
-}
+//}    public static void main(String[] args) {
+//    System.out.println("Hello World");
+//}
 }
 
