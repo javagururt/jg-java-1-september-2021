3378784992aa99b7a35c9967dcd8e61ac93f91f7
targutaj <39644968+targutaj@users.noreply.github.com>
2021-09-29 21:35:19 +0300

    lesson 3 test push

--- src/main/java/com/javaguru/vadim_babchenok/HellowWord.java
+++
@@ -1,9 +0,0 @@
-package com.javaguru.vadim_babchenok;
-
-import java.sql.SQLOutput;
-
-public class HellowWord {
-    public static void main(String[] args) {
-        System.out.println("Hy");
-    }
-}
