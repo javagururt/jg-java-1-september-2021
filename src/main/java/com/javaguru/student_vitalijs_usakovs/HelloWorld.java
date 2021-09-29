a9e0b52f66e4bcedc3a3d6ba04aea814ab77250a
vital <mailboxuv@gmail.com>
2021-09-29 22:50:20 +0300

    lesson 2 hello world Updated-22:45

--- src/main/java/com/javaguru/student_vitalijs_usakovs/HelloWorld.java
+++ src/main/java/com/javaguru/student_vitalijs_usakovs/HelloWorld.java
@@ -3,6 +3,9 @@ package com.javaguru.student_vitalijs_usakovs;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("A: Hello World!");
+        System.out.println("B: Hi");
+        System.out.println("A: How is it gong?");
+        System.out.println("B: Thx, Fine.");
     }
 }
