734d4163e694ca84afa3a71dcd1cc01bc6c58cc2
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2021-09-29 20:45:48 +0300

    lesson 1 floating number/user input examples

---
+++ src/main/java/com/javaguru/teacher/lesson_2/lessoncode/UserInputStringExample.java
@@ -0,0 +1,16 @@
+package com.javaguru.teacher.lesson_2.lessoncode;
+
+import java.util.Scanner;
+
+class UserInputStringExample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, please enter something:");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        System.out.println("Your input = " + userInput);
+        if (userInput.equalsIgnoreCase("+")) {
+            System.out.println(" WOW ");
+        }
+    }
+}
