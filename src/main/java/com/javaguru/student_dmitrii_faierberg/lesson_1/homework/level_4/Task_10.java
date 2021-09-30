a1901d14f37b8c19ce3a4d788c191f0561e37781
dimfay <dimfay27@gmail.com>
2021-09-30 08:09:37 +0300

    lesson 1

--- src/main/java/com/javaguru/student_dmitrii_faierberg/lesson_1/homework/level_4/Task_10.java
+++ src/main/java/com/javaguru/student_dmitrii_faierberg/lesson_1/homework/level_4/Task_10.java
@@ -1,7 +1,7 @@
 package com.javaguru.student_dmitrii_faierberg.lesson_1.homework.level_4;
 
 public class Task_10 {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Dmitrii Faierberg");
     }
--- src/main/java/com/javaguru/student_dmitrii_faierberg/lesson_1/homework/level_4/Task_11.java
+++ src/main/java/com/javaguru/student_dmitrii_faierberg/lesson_1/homework/level_4/Task_11.java
@@ -17,3 +17,4 @@ public class Task_11 {
                 resultSum);
     }
 }
+
