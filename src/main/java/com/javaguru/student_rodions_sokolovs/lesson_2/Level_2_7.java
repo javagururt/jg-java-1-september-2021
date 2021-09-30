6067f6ed1c60c6b7cf1b496dd5c2774a73d00f52
g3nr1 <g3nri0805@gmail.com>
2021-10-01 02:05:21 +0300

    Lesson_2 [Fix 2.7 Строчка]

--- src/main/java/com/javaguru/student_rodions_sokolovs/lesson_2/Level_2_7.java
+++ src/main/java/com/javaguru/student_rodions_sokolovs/lesson_2/Level_2_7.java
@@ -2,7 +2,8 @@
 package com.javaguru.student_rodions_sokolovs.lesson_2;
 public class Level_2_7 {
 
-    public static  void main(String[] args) {
+    public static void main(String[] args) {
+
         System.out.println("You can login!");
     }
 
