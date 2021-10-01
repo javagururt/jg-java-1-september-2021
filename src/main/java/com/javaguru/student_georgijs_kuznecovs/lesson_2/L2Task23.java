45b04654737aa3b4b84664425efb6c302578d939
George <jolytwee@gmail.com>
2021-10-01 18:36:36 +0300

    added L2Task23

---
+++ src/main/java/com/javaguru/student_georgijs_kuznecovs/lesson_2/L2Task23.java
@@ -0,0 +1,8 @@
+package com.javaguru.student_georgijs_kuznecovs.lesson_2;
+
+public class L2Task23 {     //AttentionPlease
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+
+    }
+}
