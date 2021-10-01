1f35b9701b7960e5b25f4675f6c96bb04502f3f1
OlgaT <ok.latvia@gmail.com>
2021-10-01 20:15:20 +0300

    Added lesson 2 level x task 5

---
+++ src/main/java/com/javaguru/student_Olga_T/lesson_2/level_x/task_5/Comments.java
@@ -0,0 +1,21 @@
+package com.javaguru.student_Olga_T.lesson_2.level_x.task_5;
+
+public class Comments {
+    public static void main(String[] args) {
+        //Это однострочный комментарий.
+
+        /*
+        Это многострочный коментарий.
+         */
+
+        System.out.println("//Это однострочный комментарий.");
+
+        System.out.println("/*\n" +
+                " Это многострочный комментарий.\n" +
+                "         */");
+
+
+
+        //Как это красиво вывести на экран? Ctrl + Alt + L не помогает
+    }
+}
