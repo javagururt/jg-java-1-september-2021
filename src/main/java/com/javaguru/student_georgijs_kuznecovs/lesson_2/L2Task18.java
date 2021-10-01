acb80bd30cfbf6a657fee6ce73fbbc993c13cea2
George <jolytwee@gmail.com>
2021-10-01 17:52:45 +0300

    added L2Task18

---
+++ src/main/java/com/javaguru/student_georgijs_kuznecovs/lesson_2/L2Task18.java
@@ -0,0 +1,8 @@
+package com.javaguru.student_georgijs_kuznecovs.lesson_2;
+
+public class L2Task18 {     //SizeMatters
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+}
