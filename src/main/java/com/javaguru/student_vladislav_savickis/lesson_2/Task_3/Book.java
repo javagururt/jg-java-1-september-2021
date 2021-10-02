bd589c253ad715ffe65639a89cb2ada276dd9b5e
savich12 <savich1234@gmail.com>
2021-10-02 04:00:33 +0300

    Lesson 2 v.1.0

---
+++ src/main/java/com/javaguru/student_vladislav_savickis/lesson_2/Task_3/Book.java
@@ -0,0 +1,7 @@
+package com.javaguru.student_vladislav_savickis.lesson_2.Task_3;
+
+public class Book {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/javaguru/student_vladislav_savickis/lesson_2/Task_6/Turtle.java
@@ -0,0 +1,7 @@
+package com.javaguru.student_vladislav_savickis.lesson_2.Task_6;
+
+public class Turtle {
+    public static void main(String[] args) {
+
+    }
+}
