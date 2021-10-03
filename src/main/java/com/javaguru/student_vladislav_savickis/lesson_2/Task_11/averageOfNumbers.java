76cccf9e536a265bf53b0e864eab903c59242e8b
savich12 <savich1234@gmail.com>
2021-10-03 21:27:25 +0300

    Lesson 2 v.1.3

--- src/main/java/com/javaguru/student_vladislav_savickis/lesson_2/Task_11/averageOfNumbers.java
+++ src/main/java/com/javaguru/student_vladislav_savickis/lesson_2/Task_11/averageOfNumbers.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class averageOfNumbers {
     public static void main(String[] args) {
-        System.out.println("Bonjour! Please enter first number:  ");
+        System.out.println("Bonjour! Please enter first number: ");
         Scanner input = new Scanner(System.in);
         int firstNumber = input.nextInt();
         System.out.println("Please enter second number: ");
