ddf23e7abd41b23801079b47f272fea0ceae352a
MihailMihejev <miham1606@gmail.com>
2021-10-04 23:20:52 +0300

    lesson 2 hw login!

---
+++ src/main/java/com/javaguru/student_mihails_mihejevs/lesson_2/hw_task_3.java
@@ -0,0 +1,8 @@
+package com.javaguru.student_mihails_mihejevs.lesson_2;
+/*
+Дан код, нужно исправить ошибку компиляции.
+
+class Book {
+ */
+public class hw_task_3 {
+}
---
+++ src/main/java/com/javaguru/student_mihails_mihejevs/lesson_2/hw_task_4.java
@@ -0,0 +1,10 @@
+package com.javaguru.student_mihails_mihejevs.lesson_2;
+/*
+Дан код, нужно исправить ошибку компиляции.
+
+class Phone
+
+}
+ */
+public class hw_task_4 {
+}
