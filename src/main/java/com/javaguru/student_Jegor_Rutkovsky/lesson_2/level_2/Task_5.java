7ac2dad225e793991a87311d962d5381cfc9b77d
Domerungus <domerungus@gmail.com>
2021-10-05 22:56:10 +0300

    Lesson 2 - HomeWork

---
+++ src/main/java/com/javaguru/student_Jegor_Rutkovsky/lesson_2/level_2/Task_5.java
@@ -0,0 +1,8 @@
+package com.javaguru.student_Jegor_Rutkovsky.lesson_2.level_2;
+
+class BankAccount {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/javaguru/student_vitalijs_usakovs/HelloWorld.java
+++ src/main/java/com/javaguru/student_vitalijs_usakovs/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.javaguru.student_vitalijs_usakovs
+package com.javaguru.student_vitalijs_usakovs;
 
 public class HelloWorld {
 
