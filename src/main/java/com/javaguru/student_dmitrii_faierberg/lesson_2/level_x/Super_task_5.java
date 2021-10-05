7c9144c990b67a506ac1ebd5dc8f0674514a47ad
dimfay <dimfay27@gmail.com>
2021-10-05 19:21:28 +0300

    level_x solved

---
+++ src/main/java/com/javaguru/student_dmitrii_faierberg/lesson_2/level_x/Super_task_5.java
@@ -0,0 +1,15 @@
+package com.javaguru.student_dmitrii_faierberg.lesson_2.level_x;
+
+public class Super_task_5 {
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        // System.out.println("Hello World!");
+        /* System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+         */
+        System.out.println("Goodbye world!");
+    }
+}
