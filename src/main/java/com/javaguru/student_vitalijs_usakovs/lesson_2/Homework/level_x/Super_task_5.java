4b4b9d4f7d9bafab16c2385e66362ef6a018210d
vital <mailboxuv@gmail.com>
2021-10-05 18:41:48 +0300

    lesson_2_homework_levelX_superTask3;5

--- src/main/java/com/javaguru/student_vitalijs_usakovs/lesson_2/Homework/level_x/Super_task_5.java
+++ src/main/java/com/javaguru/student_vitalijs_usakovs/lesson_2/Homework/level_x/Super_task_5.java
@@ -2,4 +2,10 @@ package com.javaguru.student_vitalijs_usakovs.lesson_2.Homework.level_x;
 /* Напишите программу и продемонстрируйте в ней использование
 однострочного и многострочного коментариев. */
 public class Super_task_5 {
+    public static void main(String[] args){
+        System.out.println("Многострочный коментарий-1 ");
+        System.out.println("Многострочный коментарий-2 ");
+        System.out.print("Однострочный коментарий-1 ");
+        System.out.print("Однострочный коментарий-2 ");
+    }
 }
