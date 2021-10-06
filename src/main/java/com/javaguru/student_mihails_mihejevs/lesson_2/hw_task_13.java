4a231833e9a54de29b7c9886082b1ac244375f96
MihailMihejev <miham1606@gmail.com>
2021-10-06 18:42:51 +0300

    lesson 2 hw login!

---
+++ src/main/java/com/javaguru/student_mihails_mihejevs/lesson_2/hw_task_13.java
@@ -0,0 +1,8 @@
+package com.javaguru.student_mihails_mihejevs.lesson_2;
+/*
+Найти в интеренете ответ на вопрос:
+Как откомпилировать код с помощью JDK компилятора javac,
+без помощи среды разработки (из консоли)?
+ */
+public class hw_task_13 {
+}
---
+++ src/main/java/com/javaguru/student_mihails_mihejevs/lesson_2/hw_task_9.java
@@ -0,0 +1,17 @@
+package com.javaguru.student_mihails_mihejevs.lesson_2;
+/*
+Напишите программу, которая запрашивает у пользователя целое число
+и печатает на консоль таблицу умножения для этого числа до 10.
+
+Test Data:
+Input a number: 8
+
+Expected Output :
+8 x 1 = 8
+8 x 2 = 16
+8 x 3 = 24
+...
+8 x 10 = 80
+ */
+public class hw_task_9 {
+}
