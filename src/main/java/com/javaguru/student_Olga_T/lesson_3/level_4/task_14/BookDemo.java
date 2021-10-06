cb657248030518dc1580e18c329408b97627af36
OlgaT <ok.latvia@gmail.com>
2021-10-06 13:06:17 +0300

    Added lesson 3 level 4 task 14

---
+++ src/main/java/com/javaguru/student_Olga_T/lesson_3/level_4/task_14/BookDemo.java
@@ -0,0 +1,9 @@
+package com.javaguru.student_Olga_T.lesson_3.level_4.task_14;
+
+public class BookDemo {
+    public static void main(String[] args) {
+        Book myBook = new Book("Principles");
+        String bookTitle = myBook.getTitle();
+        System.out.println("Book title = " + bookTitle);
+    }
+}
