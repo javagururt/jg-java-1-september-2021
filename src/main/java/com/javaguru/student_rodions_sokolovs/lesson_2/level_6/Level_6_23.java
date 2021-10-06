7c0a18079d96c2cc85a949a6f42968f3fcec034e
g3nr1 <g3nri0805@gmail.com>
2021-10-06 18:46:48 +0300

    Lesson_2 [Update]

--- src/main/java/com/javaguru/student_rodions_sokolovs/lesson_2/level_6/Level_6_23.java
+++ src/main/java/com/javaguru/student_rodions_sokolovs/lesson_2/level_6/Level_6_23.java
@@ -15,6 +15,7 @@ package com.javaguru.student_rodions_sokolovs.lesson_2.level_6;
 
 public class Level_6_23 {
     public static void main(String[] args) {
+
         System.out.println("Code outside the method will not compile!");
     }
 }
