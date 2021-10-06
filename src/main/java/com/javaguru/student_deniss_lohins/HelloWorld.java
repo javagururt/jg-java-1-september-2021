9d6d250058a308701c9bb4e1a2dad9fe536f80ce
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2021-10-06 22:22:53 +0300

    test annotation

--- src/main/java/com/javaguru/student_deniss_lohins/HelloWorld.java
+++ src/main/java/com/javaguru/student_deniss_lohins/HelloWorld.java
@@ -1,5 +1,8 @@
 package com.javaguru.student_deniss_lohins;
 
+import com.javaguru.teacher.codereview.CodeReview;
+
+@CodeReview(approved = true)
 public class HelloWorld {
     public static void main(String[] args) {
     System.out.println("Hello World!");
