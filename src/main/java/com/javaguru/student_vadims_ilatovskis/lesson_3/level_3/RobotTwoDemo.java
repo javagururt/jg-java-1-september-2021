1bb847ab997771f89bedf2875ce9aeb0dc64c613
Vadjara <vadimsilatovskis@inbox.lv>
2021-10-07 15:18:16 +0300

    Lesson_3 level_3

---
+++ src/main/java/com/javaguru/student_vadims_ilatovskis/lesson_3/level_3/RobotTwoDemo.java
@@ -0,0 +1,13 @@
+package com.javaguru.student_vadims_ilatovskis.lesson_3.level_3;
+
+public class RobotTwoDemo {
+    public static void main(String[] args) {
+        RobotTwo robot1 = new RobotTwo("Terminator");
+        robot1.sayHello();
+        robot1.sayYourName();
+
+        RobotTwo robot2 = new RobotTwo("T1000");
+        robot2.sayHello();
+        robot2.sayYourName();
+    }
+}
