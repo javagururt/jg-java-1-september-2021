15c3611ce3b8183785a3090eb1977bf88e5b9b82
OlgaT <ok.latvia@gmail.com>
2021-10-08 00:19:27 +0300

    Added lesson 3 level 6 task 28

---
+++ src/main/java/com/javaguru/student_Olga_T/lesson_3/level_6/task_28/Computer.java
@@ -0,0 +1,19 @@
+package com.javaguru.student_Olga_T.lesson_3.level_6.task_28;
+
+class Computer {
+    private String manufacturer;
+    private String model;
+
+    public Computer(String manufacturer, String model) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+}
