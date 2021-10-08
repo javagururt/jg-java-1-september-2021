d7e0bea0992b96dffb6be25e17fe69bb277d993d
George <jolytwee@gmail.com>
2021-10-08 18:47:31 +0300

    added Lesson 3 level 4

---
+++ src/main/java/com/javaguru/student_georgijs_kuznecovs/lesson_3/level_4/Book.java
@@ -0,0 +1,13 @@
+package com.javaguru.student_georgijs_kuznecovs.lesson_3.level_4;
+
+public class Book {
+    private String title;
+
+    Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+}
