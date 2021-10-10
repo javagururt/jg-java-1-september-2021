7db16c9f4182aabc5ff3b091758344499230cb93
MihailMihejev <miham1606@gmail.com>
2021-10-10 17:22:25 +0300

    12-14

--- src/main/java/com/javaguru/student_mihails_mihejevs/lesson_2/hw_task_12.java
+++ src/main/java/com/javaguru/student_mihails_mihejevs/lesson_2/hw_task_12.java
@@ -6,8 +6,10 @@ package com.javaguru.student_mihails_mihejevs.lesson_2;
 
 /*
 https://qna.habr.com/q/248892
-        /Program Files/java/jdk
+/Program Files/java/jdk
  */
+
+// не понимаю о чём речь. Какую статью прочитать?
 public class hw_task_12 {
 }
 
