2c7288218032c0fd28595dcd2251b156ed2881eb
MihailMihejev <miham1606@gmail.com>
2021-10-10 17:45:45 +0300

    18

--- src/main/java/com/javaguru/student_mihails_mihejevs/lesson_2/hw_task_18.java
+++ src/main/java/com/javaguru/student_mihails_mihejevs/lesson_2/hw_task_18.java
@@ -9,5 +9,20 @@ class SizeMatters {
     }
 }
  */
+/*??? Руслан, а почему не запускается в таком виде?
 public class hw_task_18 {
+    class SizeMatters {
+        public static void main(String[] args) {
+            System.out.println("Sorry, I have an errors.");
+            System.out.println("Please fix me!");
+        }
+    }
+}
+*/
+
+class SizeMatters {
+    public static void main(String[] args) {
+    System.out.println("Sorry, I have an errors.");
+    System.out.println("Please fix me!");
+    }
 }
