43024fe35389aa62d533f0fc0e92f42fa03c89c1
MihailMihejev <miham1606@gmail.com>
2021-10-10 13:15:51 +0300

    lesson 2 hw 1-2

--- src/main/java/com/javaguru/student_mihails_mihejevs/lesson_2/hw_task_3.java
+++ src/main/java/com/javaguru/student_mihails_mihejevs/lesson_2/hw_task_3.java
@@ -4,5 +4,13 @@ package com.javaguru.student_mihails_mihejevs.lesson_2;
 
 class Book {
  */
+
+// речь идёт о }?
+// почему в класе появилось .java?
+// почему в класе появились подклассы, которые я не создавал?
+
 public class hw_task_3 {
+}
+class Book {
+
 }
