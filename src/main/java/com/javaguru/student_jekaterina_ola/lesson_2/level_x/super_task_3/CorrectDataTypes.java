754cd68730cb2e3698ad9ac0434c9052e1b4cde7
kote6ka <jekaterina.ola@gmail.com>
2021-10-11 21:26:14 +0300

    lesson 2 super task 3

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_2/level_x/super_task_3/CorrectDataTypes.java
@@ -0,0 +1,16 @@
+package com.javaguru.student_jekaterina_ola.lesson_2.level_x.super_task_3;
+
+public class CorrectDataTypes {
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        double numberTwo = 20.0;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+        double sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+    }
+}
