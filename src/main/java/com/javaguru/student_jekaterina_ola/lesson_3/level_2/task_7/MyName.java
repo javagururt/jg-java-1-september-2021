82a2bf9dc894653008af732305a9039b2327287a
kote6ka <jekaterina.ola@gmail.com>
2021-10-11 23:24:45 +0300

    lesson 3 task 7

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_3/level_2/task_7/MyName.java
@@ -0,0 +1,8 @@
+package com.javaguru.student_jekaterina_ola.lesson_3.level_2.task_7;
+
+public class MyName {
+    public static void main(String[] args){
+        String myName = "Jekaterina";
+        System.out.println(myName);
+    }
+}
