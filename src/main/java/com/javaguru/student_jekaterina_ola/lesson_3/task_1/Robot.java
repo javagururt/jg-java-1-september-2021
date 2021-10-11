462050919a88e64b6038bcdf13ca7244b166b1b8
kote6ka <jekaterina.ola@gmail.com>
2021-10-11 22:56:10 +0300

    lesson 3 task 1

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_3/task_1/Robot.java
@@ -0,0 +1,10 @@
+package com.javaguru.student_jekaterina_ola.lesson_3.task_1;
+
+public class Robot {
+        public void sayHello () {
+            System.out.println("Hello");
+        }
+        public void sayYourName () {
+            System.out.println("My name is Leo");
+        }
+    }
