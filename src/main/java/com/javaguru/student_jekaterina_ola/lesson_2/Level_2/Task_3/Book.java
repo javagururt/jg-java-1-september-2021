fd8b3dbcc7b722bf5f02f18b1c5a4f3143845b6d
kote6ka <jekaterina.ola@gmail.com>
2021-10-11 21:10:13 +0300

    lesson 2 supertask 1

--- src/main/java/com/javaguru/student_jekaterina_ola/lesson_2/Level_2/Task_3/Book.java
+++
@@ -1,7 +0,0 @@
-package com.javaguru.student_jekaterina_ola.lesson_2.Level_2.Task_3;
-
-import com.javaguru.teacher.codereview.CodeReview;
-
-@CodeReview(approved = true)
-public class Book {
-}
---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_2/level_x/BankAccount.java
@@ -0,0 +1,4 @@
+package com.javaguru.student_jekaterina_ola.lesson_2.level_x;
+
+public class BankAccount {
+}
