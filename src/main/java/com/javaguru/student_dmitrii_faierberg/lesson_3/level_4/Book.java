64d9378d8bda74826b7e854b32fa19246ca91acf
dimfay <dimfay27@gmail.com>
2021-10-11 10:46:45 +0300

    lesson 3 levels 1-5

---
+++ src/main/java/com/javaguru/student_dmitrii_faierberg/lesson_3/level_4/Book.java
@@ -0,0 +1,15 @@
+package com.javaguru.student_dmitrii_faierberg.lesson_3.level_4;
+
+class Book {
+
+    private String title;
+
+    Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+}
