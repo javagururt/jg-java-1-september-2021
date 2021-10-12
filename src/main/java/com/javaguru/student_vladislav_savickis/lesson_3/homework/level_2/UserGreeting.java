0248e75a5753194010ecefc16a59b7fc89200fdf
savich12 <savich1234@gmail.com>
2021-10-13 01:22:25 +0300

    Lesson 3 Level 2

---
+++ src/main/java/com/javaguru/student_vladislav_savickis/lesson_3/homework/level_2/UserGreeting.java
@@ -0,0 +1,9 @@
+package com.javaguru.student_vladislav_savickis.lesson_3.homework.level_2;
+
+public class UserGreeting {
+    public static void main(String[] args) {
+        String userName = "Vladislav";
+        String greeting = "Hi " + userName + "!";
+        System.out.println(greeting);
+    }
+}
