79c7c5cd298286e7541efdd49b8a1b0c420009c3
kote6ka <jekaterina.ola@gmail.com>
2021-10-12 22:33:36 +0300

    lesson 3 task 15

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_3/level_4/task_15/Phone.java
@@ -0,0 +1,23 @@
+package com.javaguru.student_jekaterina_ola.lesson_3.level_4.task_15;
+
+/*Найти ошибку в коде.
+        Исправить и запустить программу.
+        Менять можно только класс Phone.
+*/
+
+class Phone {
+
+    private String model;
+
+    public Phone(String newModel) {
+        this.model = newModel;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+}
+
+
+
