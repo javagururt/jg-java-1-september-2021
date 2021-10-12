3b239b864b5f97082185363b040c1ece478121e0
Domerungus <domerungus@gmail.com>
2021-10-12 21:41:02 +0300

    Lesson 3

---
+++ src/main/java/com/javaguru/student_Jegor_Rutkovsky/lesson_3/level_5/DogDemo.java
@@ -0,0 +1,16 @@
+package com.javaguru.student_Jegor_Rutkovsky.lesson_3.level_5;
+
+public class DogDemo {
+    public static void main(String[] args) {
+        Dog myDog = new Dog("Barry", 5);
+        myDog.voice();
+        System.out.println("Dogs age is " +myDog.dogAge);
+        myDog.happyBirthday();
+        System.out.println("Dogs age is " +myDog.dogAge);
+        myDog.getAge();
+
+
+        
+
+        }
+}
