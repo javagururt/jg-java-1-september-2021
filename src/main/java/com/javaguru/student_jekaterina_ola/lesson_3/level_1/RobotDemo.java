4f218ea0e73dec9f49a77ebe2e51f2bd245ce96e
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2021-10-13 18:44:11 +0300

    code review

--- src/main/java/com/javaguru/student_jekaterina_ola/lesson_3/level_1/RobotDemo.java
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_3/level_1/RobotDemo.java
@@ -1,8 +1,10 @@
 package com.javaguru.student_jekaterina_ola.lesson_3.level_1;
 
 import com.javaguru.teacher.codereview.CodeReview;
+import com.javaguru.teacher.codereview.CodeReviewComment;
 
-@CodeReview(approved = true)
+@CodeReview(approved = false)
+@CodeReviewComment(teacher = "в конструктор необходимо подать аргумент. А так же его использовать в классе Robot.")
 public class RobotDemo {
     public static void main(String[] args) {
         Robot robot = new Robot();
