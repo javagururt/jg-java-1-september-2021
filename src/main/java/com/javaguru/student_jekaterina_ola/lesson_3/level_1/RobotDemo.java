75fe8c2981a89a6cc470e3a286cfe0a66e19e25c
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2021-10-13 18:44:28 +0300

    Commented error

--- src/main/java/com/javaguru/student_jekaterina_ola/lesson_3/level_1/RobotDemo.java
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_3/level_1/RobotDemo.java
@@ -7,14 +7,14 @@ import com.javaguru.teacher.codereview.CodeReviewComment;
 @CodeReviewComment(teacher = "в конструктор необходимо подать аргумент. А так же его использовать в классе Robot.")
 public class RobotDemo {
     public static void main(String[] args) {
-        Robot robot = new Robot();
-        robot.sayHello();
-        robot.sayYourName();
-        System.out.println();
-
-        Robot robot1 = new Robot();
-        robot1.sayHello();
-        robot1.sayYourName();
-        System.out.println();
+//        Robot robot = new Robot();
+//        robot.sayHello();
+//        robot.sayYourName();
+//        System.out.println();
+//
+//        Robot robot1 = new Robot();
+//        robot1.sayHello();
+//        robot1.sayYourName();
+//        System.out.println();
         }
     }
