a396f737635972eada12e17f46e5570a8e828fc9
targutaj <39644968+targutaj@users.noreply.github.com>
2021-10-13 20:58:04 +0300

    level 5 task 21 - student Vadim Babcenok

--- src/main/java/com/javaguru/student_vadim_babchenok/lesson3/level_5/Dog.java
+++ src/main/java/com/javaguru/student_vadim_babchenok/lesson3/level_5/Dog.java
@@ -4,20 +4,38 @@ import com.javaguru.teacher.codereview.CodeReview;
 
 @CodeReview(approved = true)
 public class Dog {
+    //polja klassa
     String name;
     int age;
+    String color;
+
     //konstruktor
-    public Dog(String dogname,int dogage){
+    public Dog(String dogname,int dogage,String dogcolor){
      this.name=dogname;
      this.age=dogage;
+     this.color=dogcolor;
     }
 
+    public String getColor() {
+        return color;
+    }
+
+    //metod klassa
     public String getName() {
         return this.name;
     }
 
+    //metod klassa
     public void voice() {
         System.out.println("Собаке по кличке "+ this.name + " целых " + this.age+ " лет!");
+    }
 
+    //metod klassa
+    public void happyBirthday() {
+        this.age=this.age+1;
+    }
+
+    public void color() {
+        System.out.println("Собакa по кличке "+ this.name + "  стала цвета - " + this.color );
     }
 }
