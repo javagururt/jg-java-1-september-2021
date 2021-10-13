05db95c8a8a628bd31ece9749dbb3057006e49a3
Domerungus <domerungus@gmail.com>
2021-10-13 18:51:19 +0300

    Lesson 3 - 13.10.2021

--- src/main/java/com/javaguru/student_Jegor_Rutkovsky/lesson_3/level_5/DogDemo.java
+++ src/main/java/com/javaguru/student_Jegor_Rutkovsky/lesson_3/level_5/DogDemo.java
@@ -2,12 +2,14 @@ package com.javaguru.student_Jegor_Rutkovsky.lesson_3.level_5;
 
 public class DogDemo {
     public static void main(String[] args) {
-        Dog myDog = new Dog("Barry", 5);
+        Dog myDog = new Dog("Barry", 5, "Black");
         myDog.voice();
         System.out.println("Dogs age is " +myDog.dogAge);
         myDog.happyBirthday();
         System.out.println("Dogs age is " +myDog.dogAge);
         myDog.getAge();
+        myDog.changeColor("White");
+        myDog.voice();
 
 
         
