c0f0b88282f30c178d61ac1284550e8a5a5fbc6a
savich12 <savich1234@gmail.com>
2021-10-14 04:35:59 +0300

    Lesson 3 Level 6.5

---
+++ src/main/java/com/javaguru/student_vladislav_savickis/lesson_3/homework/level_6/ProductDemo.java
@@ -0,0 +1,8 @@
+package com.javaguru.student_vladislav_savickis.lesson_3.homework.level_6;
+
+public class ProductDemo {
+    public static void main(String[] args) {
+        Product product = new Product("Toletes papirs", 1.99, 0.50);
+        product.printInformation();
+    }
+}
