320efb141957dee392807b758dd5647a396dd85d
dimfay <dimfay27@gmail.com>
2021-10-15 08:55:41 +0300

    lesson 3 level 6

---
+++ src/main/java/com/javaguru/student_dmitrii_faierberg/lesson_3/level_6/Task_24/Bicycle.java
@@ -0,0 +1,33 @@
+package com.javaguru.student_dmitrii_faierberg.lesson_3.level_6.Task_24;
+
+class Bicycle {
+    int transmission = 1;
+    boolean areBreakesOn = false;
+    boolean isRiding = false;
+
+    void setTransmission(int transmission){
+        if (transmission >= 1 && transmission <= 11){
+            System.out.println("The speed was changed from " + this.transmission + " to "
+            + transmission);
+            this.transmission = transmission;
+        }
+        else System.err.println("This speed does not exist");
+    }
+    void ride(){
+        if (isRiding) System.out.println("The bike is moving already");
+        else {
+            isRiding = true;
+            areBreakesOn = false;
+            System.out.println("We are riding");
+        }
+    }
+
+    void stop(){
+        if (areBreakesOn) System.out.println("The bike is stopped already");
+        else {
+            areBreakesOn = true;
+            isRiding = false;
+            System.out.println("The bike is stopped");
+        }
+    }
+}
---
+++ src/main/java/com/javaguru/student_dmitrii_faierberg/lesson_3/level_6/Task_24/ParrotDemo.java
@@ -0,0 +1,11 @@
+package com.javaguru.student_dmitrii_faierberg.lesson_3.level_6.Task_24;
+
+class ParrotDemo {
+    public static void main(String[] args) {
+        Parrot parrot = new Parrot("Kesha");
+        parrot.tweet();
+        parrot.feed();
+        parrot.tweet();
+        parrot.feed();
+    }
+}
