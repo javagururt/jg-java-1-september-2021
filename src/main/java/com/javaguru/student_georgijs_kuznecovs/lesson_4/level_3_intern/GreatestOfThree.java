c2d922fc7a854dd882cfee31388adcce06c25548
George <jolytwee@gmail.com>
2021-10-16 22:12:20 +0300

    added Lesson 4 level 4

--- src/main/java/com/javaguru/student_georgijs_kuznecovs/lesson_4/level_3_intern/GreatestOfThree.java
+++ src/main/java/com/javaguru/student_georgijs_kuznecovs/lesson_4/level_3_intern/GreatestOfThree.java
@@ -15,9 +15,23 @@ class GreatestOfThree {
 
         if (firstNum > secondNum && firstNum > thirdNum){
             System.out.println("The greatest number is: " + firstNum);
+        }else if (firstNum == secondNum && firstNum > thirdNum){
+            System.out.println("The greatest number is: " + firstNum);
+        }else if (firstNum == thirdNum && firstNum > secondNum){
+            System.out.println("The greatest number is: " + firstNum);
         }else if (secondNum > firstNum && secondNum > thirdNum){
             System.out.println("The greatest number is: " + secondNum);
-        }else
+        }else if (firstNum == secondNum && secondNum > thirdNum){
+            System.out.println("The greatest number is: " + secondNum);
+        }else if (secondNum == thirdNum && secondNum > firstNum){
+            System.out.println("The greatest number is: " + secondNum);
+        }else if (thirdNum > firstNum && thirdNum > secondNum) {
             System.out.println("The greatest number is: " + thirdNum);
+        }else if (firstNum == thirdNum && thirdNum > secondNum){
+            System.out.println("The greatest number is: " + thirdNum);
+        }else if (secondNum == thirdNum && thirdNum > firstNum){
+            System.out.println("The greatest number is: " + thirdNum);
+        } else
+            System.out.println("Numbers are equal");
     }
 }
