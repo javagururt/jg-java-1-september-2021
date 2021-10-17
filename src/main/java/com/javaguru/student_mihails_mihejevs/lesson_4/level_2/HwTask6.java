0698d9753f4129b1e51d05631c566633af1b2184
MihailMihejev <miham1606@gmail.com>
2021-10-17 22:13:35 +0300

    HW_4_2

--- src/main/java/com/javaguru/student_mihails_mihejevs/lesson_4/level_2/HwTask6.java
+++ src/main/java/com/javaguru/student_mihails_mihejevs/lesson_4/level_2/HwTask6.java
@@ -2,7 +2,7 @@ package com.javaguru.student_mihails_mihejevs.lesson_4.level_2;
 
 import java.util.Scanner;
 
-public class HwTask5 {
+public class HwTask6 {
     public static void main (String[] args) {
         System.out.println("Hello!");
         System.out.println("Please enter two whole number.");
@@ -11,11 +11,12 @@ public class HwTask5 {
         int firstNumber = input.nextInt();
         System.out.println("Please enter second number: ");
         int secondNumber = input.nextInt();
+        System.out.println();
 
-            if (firstNumber > secondNumber) {
-                System.out.println("More: " + firstNumber);
-            } else {
-                System.out.println("More: " + secondNumber);
-            }
+        if (firstNumber < secondNumber) {
+            System.out.println("Fewer: " + firstNumber);
+        } else {
+            System.out.println("Fewer: " + secondNumber);
+        }
     }
 }
--- src/main/java/com/javaguru/student_mihails_mihejevs/lesson_4/level_2/HwTask7.java
+++ src/main/java/com/javaguru/student_mihails_mihejevs/lesson_4/level_2/HwTask7.java
@@ -2,7 +2,7 @@ package com.javaguru.student_mihails_mihejevs.lesson_4.level_2;
 
 import java.util.Scanner;
 
-public class HwTask5 {
+public class HwTask7 {
     public static void main (String[] args) {
         System.out.println("Hello!");
         System.out.println("Please enter two whole number.");
@@ -12,10 +12,11 @@ public class HwTask5 {
         System.out.println("Please enter second number: ");
         int secondNumber = input.nextInt();
 
-            if (firstNumber > secondNumber) {
-                System.out.println("More: " + firstNumber);
-            } else {
-                System.out.println("More: " + secondNumber);
-            }
+        if (firstNumber == secondNumber) {
+            System.out.println("\"Numbers are equals\"");
+        } else {
+            System.out.println("\"Numbers are different\"");
+        }
     }
 }
+
