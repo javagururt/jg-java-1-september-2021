ed10dd452f2dd80691471e7bdd0d6a1d5e8bd037
MihailMihejev <miham1606@gmail.com>
2021-10-17 17:54:50 +0300

    HW_4_1_1-2

--- src/main/java/com/javaguru/student_mihails_mihejevs/lesson_4/level_1/HwTask2.java
+++ src/main/java/com/javaguru/student_mihails_mihejevs/lesson_4/level_1/HwTask2.java
@@ -1,10 +1,9 @@
 package com.javaguru.student_mihails_mihejevs.lesson_4.level_1;
 
-
 import java.util.Scanner;
 
-public class HwTask1 {
-    public static void main (String[] args) {
+public class HwTask2 {
+    public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("Please enter whole number: ");
         Scanner input = new Scanner(System.in);
@@ -12,8 +11,10 @@ public class HwTask1 {
 
         if (enteredNumber > 0) {
             System.out.println("positive");
-        } else {
+        } else if (enteredNumber < 0) {
             System.out.println("negative");
-                    }
+        } else {
+            System.out.println("It`s zero");
+        }
     }
 }
