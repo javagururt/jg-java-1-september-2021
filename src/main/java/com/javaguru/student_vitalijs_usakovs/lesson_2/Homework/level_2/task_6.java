3a2e2790ee9f7c0a44c0957b32fdd73e122348ea
Vitalijs46 <mailboxuv@gmail.com>
2021-10-17 22:05:53 +0300

    lesson_3

--- src/main/java/com/javaguru/student_vitalijs_usakovs/lesson_2/Homework/level_2/task_6.java
+++ src/main/java/com/javaguru/student_vitalijs_usakovs/lesson_2/Homework/level_2/task_6.java
@@ -1,14 +1,6 @@
 package com.javaguru.student_vitalijs_usakovs.lesson_2.Homework.level_2;
-/*
 
-public class task_6 {
-    class Turtle {
-
-        public static void main(String[] args){
-
-    }
-
+class Turtle {
+	public static void main(String[] args){
 }
-
 }
-*/
--- src/main/java/com/javaguru/student_vitalijs_usakovs/lesson_2/Homework/level_2/task_8.java
+++ src/main/java/com/javaguru/student_vitalijs_usakovs/lesson_2/Homework/level_2/task_8.java
@@ -1,12 +1,13 @@
 package com.javaguru.student_vitalijs_usakovs.lesson_2.Homework.level_2;
 
 import java.util.Scanner;
-
+/*
 public class task_8 {
-  /*  public static void main(String[] args){
+    public static void main(String[] args){
         System.out.println("Hello! Please enter your First name");
         Scanner input = new Scanner(System.in);
-        char firstname = input.????();
+        String firstname = input.toString();
         System.out.println("Hello " + firstname);
-    }*/
-}
+    }
+}//double radius = input.nextDouble();
+*/
