a27924a0e490fc178ef818a088e844e9d2cc23a5
ZiwtinjaLV <Julija0815>
2021-10-17 21:53:48 +0300

    Lesson 4 homeworks level 2-6

---
+++ src/main/java/com/javaguru/student_deniss_lohins/lesson_4/level_3/AscendingOrDescendingOrder.java
@@ -0,0 +1,30 @@
+package com.javaguru.student_deniss_lohins.lesson_4.level_3;
+
+import java.util.Scanner;
+
+class AscendingOrDescendingOrder {
+
+    private int firstNumber;
+    private int secondNumber;
+    private int thirdNumber;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please, enter first number: ");
+        int firstNumber = scanner.nextInt();
+        System.out.print("Please, enter second number: ");
+        int secondNumber = scanner.nextInt();
+        System.out.print("Please, enter third number: ");
+        int thirdNumber = scanner.nextInt();
+
+        if (firstNumber < secondNumber && secondNumber < thirdNumber){
+            System.out.println("increasing");
+        }else if(firstNumber > secondNumber && secondNumber > thirdNumber ){
+            System.out.println("decreasing");
+        }else{
+            System.out.println("Neither increasing or decreasing order");
+        }
+
+    }
+}
+
