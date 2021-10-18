e11834ef4012148f39f6fd45435b74602010a7ca
Vitalijs46 <mailboxuv@gmail.com>
2021-10-18 22:06:28 +0300

    lesson_3_HomeWork_1-10

--- src/main/java/com/javaguru/student_vitalijs_usakovs/lesson_3/Homework/Book.java
+++ src/main/java/com/javaguru/student_vitalijs_usakovs/lesson_3/Homework/Book.java
@@ -1,2 +1,18 @@
-package com.javaguru.student_vitalijs_usakovs.lesson_3.Homework;class Book {
+package com.javaguru.student_vitalijs_usakovs.lesson_3.Homework;
+
+class Book {
+  /*  private String title;
+    public void Book(){
+        this.title = newTitle;
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    class BookDemo {
+        public static void main(String[] args) {
+            Book myBook = new Book();
+            String bookTitle = myBook.getTitle();
+            System.out.println("Book title = " + bookTitle);
+        }
+    }*/
 }
--- src/main/java/com/javaguru/student_vitalijs_usakovs/lesson_3/Homework/Robot.java
+++ src/main/java/com/javaguru/student_vitalijs_usakovs/lesson_3/Homework/Robot.java
@@ -1,4 +1,18 @@
 package com.javaguru.student_vitalijs_usakovs.lesson_3.Homework;
 
-class Robot {}
+public class Robot {
+    private String name;
 
+    public void sayHello(){
+        System.out.println("Hello!");
+    }
+
+    public Robot(String robotName) {
+        this.name = robotName;
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is " + this.name);
+    }
+
+ }
