61eb18d9a46e7f022aad855454e8c20ed77ed5c3
kote6ka <jekaterina.ola@gmail.com>
2021-10-18 21:59:25 +0300

    lesson 3 task 25

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_3/level_6/task_25/CircleDemo.java
@@ -0,0 +1,12 @@
+package com.javaguru.student_jekaterina_ola.lesson_3.level_6.task_25;
+
+import java.util.Scanner;
+
+public class CircleDemo {
+    public static void main(String[] args) {
+        Circle firstCircle = new Circle(22);
+        firstCircle.calculatedArea();
+        firstCircle.finalResult();
+    }
+}
+
