938c0972a6baa0b3c4e84a3b83422775b8588215
targutaj <39644968+targutaj@users.noreply.github.com>
2021-10-18 16:54:56 +0300

    level 4 task 8 - student Vadim Babcenok home

--- src/main/java/com/javaguru/student_vadim_babchenok/lesson4_home/task_8/DifferentNumber.java
+++ src/main/java/com/javaguru/student_vadim_babchenok/lesson4_home/task_8/DifferentNumber.java
@@ -2,10 +2,7 @@ package com.javaguru.student_vadim_babchenok.lesson4_home.task_8;
 
 import java.util.Scanner;
 
-/*Напишите программу на Java, которая принимает три числа и печатает:
-        - «Все числа равны», если все три числа равны,
-        - «Все числа разные», если все три числа разные
-        - Иначе «ни все не равны, ни различны».*/
+
 public class DifferentNumber {
     public static void main(String[] args) {
         System.out.println("Enter three numbers and press start!");
@@ -19,9 +16,9 @@ public class DifferentNumber {
         }
         else if (number != numberTwo &&
                 numberTwo != numberThree &&
-                number != numberThree ) {                        //44,43,42 firstNumber != secondNumber &&
-            System.out.println("All numbers are different");                 //secondNumber != thirdNumber &&
-                                                                             //firstNumber != thirdNumber
+                number != numberThree ) {                        //44,43,42
+            System.out.println("All numbers are different");
+
         }
         else {
             System.out.println("Neither all are equal or different");//44,44,43
