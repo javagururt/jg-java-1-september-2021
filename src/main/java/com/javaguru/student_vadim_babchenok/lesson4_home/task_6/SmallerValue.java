ecb549f550e4528ef5bbc8062fe3e9604b6f417c
targutaj <39644968+targutaj@users.noreply.github.com>
2021-10-18 15:10:28 +0300

    level 4 task 6 - student Vadim Babcenok home

---
+++ src/main/java/com/javaguru/student_vadim_babchenok/lesson4_home/task_6/SmallerValue.java
@@ -0,0 +1,24 @@
+package com.javaguru.student_vadim_babchenok.lesson4_home.task_6;
+
+import java.util.Scanner;
+
+/*Написать программу, которая запрашивает у пользователя
+        два целых числа и выводит на консоль наименьшее из них.*/
+public class SmallerValue {
+    public static void main(String[] args) {
+        System.out.println("Необходимо ввести два числа. Из двух чисел будет указано меньшее!");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        int numberTwo = input.nextInt();
+        if (number < numberTwo ){
+            System.out.println("число " + number +" меньше чем число "+ numberTwo);
+        }
+        else if (number > numberTwo ){
+            System.out.println ("число " + numberTwo + " меньше чем число "+ number);}
+        else
+        {
+            System.out.println("Числа равны");
+            }
+        }
+    }
+
