de27739903482b07ee815bb5c07f609938d98426
Vitalijs46 <mailboxuv@gmail.com>
2021-10-18 23:45:46 +0300

    lesson_3_HomeWork_18

---
+++ src/main/java/com/javaguru/student_vitalijs_usakovs/lesson_3/Homework/Dog.java
@@ -0,0 +1,39 @@
+package com.javaguru.student_vitalijs_usakovs.lesson_3.Homework;
+
+import java.util.Scanner;
+/*
+Тема: создание класса Dog.
+Создать класс собака (Dog) и продемонстрировать его работу с ним в программе.
+Бизнес требования:
+- У собаки должна быть кличка.
+- Кличку собаке дают в момент создания и потом ее изменить нельзя.
+- Собака должна уметь выполнять команду голос (voice()),
+при выполнении этой команды на консоль должно выводиться кличка собаки три раза.
+Создать отдельный класс DogDemo, объявить в нём main() метод и продемонстрировать использование класса собака (Dog).
+ */
+class Dog {
+    String name;
+    String bark;
+    public Dog(String dogName){
+        this.name = dogName;
+    }
+    public void dogName(){
+        System.out.println(this.name);
+    }
+    public void dogBark(){
+        System.out.println("*" + this.name);
+    }
+}
+
+class DogDemo {
+    public static void main(String[] args) {
+    System.out.println("Choose Dog name: ");
+        Scanner input = new Scanner(System.in);
+        String nameDog = input.next();
+
+        Dog myDog = new Dog(nameDog);
+        myDog.dogBark();
+        myDog.dogBark();
+        myDog.dogBark();
+    }
+}
