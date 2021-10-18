508391f8f6770afe6a6951046678c6d8ee4b057f
kote6ka <jekaterina.ola@gmail.com>
2021-10-18 23:12:21 +0300

    lesson 3 task 27

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_3/level_6/task_27/BankAccount.java
@@ -0,0 +1,18 @@
+package com.javaguru.student_jekaterina_ola.lesson_3.level_6.task_27;
+
+public class BankAccount {
+
+        private String owner;
+        private int money;
+
+        public BankAccount(String owner, int money) {
+            this.owner = owner;
+            this.money = money;
+        }
+
+        public String getOwner() {return this.owner;}
+
+        public int getMoney() {return this.money;}
+
+    }
+
---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_3/level_6/task_27/BankAccountDemo.java
@@ -0,0 +1,12 @@
+package com.javaguru.student_jekaterina_ola.lesson_3.level_6.task_27;
+
+public class BankAccountDemo {
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount("Viktor", 1000000);
+        String ownerFirstName = bankAccount.getOwner();
+        int moneyAmount = bankAccount.getMoney();
+        System.out.println("Owner = " + ownerFirstName);
+        System.out.println("Money = " + moneyAmount);
+    }
+    }
+
