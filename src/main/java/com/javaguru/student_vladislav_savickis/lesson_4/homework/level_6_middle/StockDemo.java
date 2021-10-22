ff940f0f730300dffd8ac2e9be71bc79c2b223ec
savich12 <savich1234@gmail.com>
2021-10-23 01:40:26 +0300

    Lesson 4 Level 6

---
+++ src/main/java/com/javaguru/student_vladislav_savickis/lesson_4/homework/level_6_middle/StockDemo.java
@@ -0,0 +1,8 @@
+package com.javaguru.student_vladislav_savickis.lesson_4.homework.level_6_middle;
+
+class StockDemo {
+    public static void main(String[] args) {
+        Stock stock = new Stock();
+        stock.updatePrice("Google", 3000.0);
+    }
+}
