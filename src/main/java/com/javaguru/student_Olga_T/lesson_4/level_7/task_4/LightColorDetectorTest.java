22961163dd0214e073a1354747ca25f4c9a0f2c6
OlgaT <ok.latvia@gmail.com>
2021-10-24 23:04:32 +0300

    Added lesson 4 level 7 task 4

---
+++ src/main/java/com/javaguru/student_Olga_T/lesson_4/level_7/task_4/LightColorDetectorTest.java
@@ -0,0 +1,67 @@
+package com.javaguru.student_Olga_T.lesson_4.level_7.task_4;
+
+class LightColorDetectorTest {
+    public static void main(String[] args) {
+        LightColorDetectorTest lightColorDetector = new LightColorDetectorTest();
+
+        lightColorDetector.testViolet(405);
+        lightColorDetector.testBlue(475);
+        lightColorDetector.testGreen(505);
+        lightColorDetector.testYellow(585);
+        lightColorDetector.testOrange(619);
+        lightColorDetector.testRed(620);
+        lightColorDetector.testInvisible(760);
+    }
+
+    public void testViolet(int wavelength) {
+        LightColorDetector lightColorDetector = new LightColorDetector();
+        String result = lightColorDetector.detect(wavelength);
+        allTests(result.equals("Violet"), "testViolet");
+    }
+
+    public void testBlue(int wavelength) {
+        LightColorDetector lightColorDetector = new LightColorDetector();
+        String result = lightColorDetector.detect(wavelength);
+        allTests(result.equals("Blue"), "testBlue");
+    }
+
+    public void testGreen(int wavelength) {
+        LightColorDetector lightColorDetector = new LightColorDetector();
+        String result = lightColorDetector.detect(wavelength);
+        allTests(result.equals("Green"), "testGreen");
+    }
+
+    public void testYellow(int wavelength) {
+        LightColorDetector lightColorDetector = new LightColorDetector();
+        String result = lightColorDetector.detect(wavelength);
+        allTests(result.equals("Yellow"), "testYellow");
+    }
+
+    public void testOrange(int wavelength) {
+        LightColorDetector lightColorDetector = new LightColorDetector();
+        String result = lightColorDetector.detect(wavelength);
+        allTests(result.equals("Orange"), "testOrange");
+    }
+
+    public void testRed(int wavelength) {
+        LightColorDetector lightColorDetector = new LightColorDetector();
+        String result = lightColorDetector.detect(wavelength);
+        allTests(result.equals("Red"), "testRed");
+    }
+
+    public void testInvisible(int wavelength) {
+        LightColorDetector lightColorDetector = new LightColorDetector();
+        String result = lightColorDetector.detect(wavelength);
+        allTests(result.equals("Invisible Light"), "testInvisible");
+
+    }
+
+    public void allTests(boolean condition, String testName) {
+        if (condition) {
+            System.out.println(testName + " OK");
+        } else {
+            System.out.println(testName + " FAIL");
+        }
+    }
+
+}
