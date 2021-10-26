3be017eda383a67d39c3f43094290aec290938bc
targutaj <39644968+targutaj@users.noreply.github.com>
2021-10-26 10:51:22 +0300

    level 5 task 9 - student Vadim Babcenok home

---
+++ src/main/java/com/javaguru/student_vadim_babchenok/lesson5home/task9_home/ArrayLength3.java
@@ -0,0 +1,10 @@
+package com.javaguru.student_vadim_babchenok.lesson5home.task9_home;
+
+import static java.lang.System.*;
+
+public class ArrayLength3 {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        out.println(numbers.length);
+    }
+}
