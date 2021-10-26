1524dd9a294ce35cb03581f8e0e1614d65df9962
targutaj <39644968+targutaj@users.noreply.github.com>
2021-10-26 09:44:10 +0300

    level 5 task 5 - student Vadim Babcenok home

---
+++ src/main/java/com/javaguru/teacher/lesson_5/homework/level_1_intern/ArrayDefinition5.java
@@ -0,0 +1,7 @@
+package com.javaguru.teacher.lesson_5.homework.level_1_intern;
+
+public class ArrayDefinition5 {
+    public static void main(String[] args) {
+        int [] numbers = {1, 2, 3,4 ,5 };
+    }
+}
