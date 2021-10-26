4c8835269ffcb5fe99339b69633ef4bfdbe57979
targutaj <39644968+targutaj@users.noreply.github.com>
2021-10-26 10:34:54 +0300

    level 5 task 7 - student Vadim Babcenok home

---
+++ src/main/java/com/javaguru/student_vadim_babchenok/lesson5home/task7_home/ArrayLength1.java
@@ -0,0 +1,9 @@
+package com.javaguru.student_vadim_babchenok.lesson5home.task7_home;
+
+public class ArrayLength1 {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        numbers[2]=1;
+        System.out.println(numbers[2]);
+    }
+}
