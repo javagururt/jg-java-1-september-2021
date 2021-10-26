8335558296ea874b18153c9d73aae3e129e34733
targutaj <39644968+targutaj@users.noreply.github.com>
2021-10-26 10:19:28 +0300

    level 5 task 6 - student Vadim Babcenok home

---
+++ src/main/java/com/javaguru/student_vadim_babchenok/lesson5home/task6_home/ArrayDefinition6.java
@@ -0,0 +1,7 @@
+package com.javaguru.student_vadim_babchenok.lesson5home.task6_home;
+
+public class ArrayDefinition6 {
+    public static void main(String[] args) {
+        int [] numbers = {1, 2, 3, 4 };
+    }
+}
