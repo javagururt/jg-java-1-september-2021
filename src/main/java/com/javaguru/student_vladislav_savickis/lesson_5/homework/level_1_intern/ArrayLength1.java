00f28efacbd1e9e378ee003f80bb0c21ff7f92f6
savich12 <savich1234@gmail.com>
2021-10-26 17:37:19 +0300

    Lesson 5 Level 1

---
+++ src/main/java/com/javaguru/student_vladislav_savickis/lesson_5/homework/level_1_intern/ArrayLength1.java
@@ -0,0 +1,10 @@
+package com.javaguru.student_vladislav_savickis.lesson_5.homework.level_1_intern;
+
+class ArrayLength1 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        numbers[1] = 2;
+        System.out.println(numbers[1]);
+    }
+}
