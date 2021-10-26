6b20134cef003e88deeca60184347f2320232b4f
savich12 <savich1234@gmail.com>
2021-10-26 19:35:26 +0300

    Lesson 5 Level 3

---
+++ src/main/java/com/javaguru/student_vladislav_savickis/lesson_5/homework/level_3_junior/ForLoopDefinition2.java
@@ -0,0 +1,9 @@
+package com.javaguru.student_vladislav_savickis.lesson_5.homework.level_3_junior;
+
+class ForLoopDefinition2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
