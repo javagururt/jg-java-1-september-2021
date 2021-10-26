97a4f0a415154159ca08343f05572607e80df576
targutaj <39644968+targutaj@users.noreply.github.com>
2021-10-26 09:16:33 +0300

    level 5 task 1 - student Vadim Babcenok home

---
+++ src/main/java/com/javaguru/student_vadim_babchenok/lesson5home/task1home/ArrayDefinition1.java
@@ -0,0 +1,8 @@
+package com.javaguru.student_vadim_babchenok.lesson5home.task1home;
+//Найдите ошибку в программе и исправить её:
+public class ArrayDefinition1 {
+
+    public static void main(String[] args) {
+        int [] numbers = new int[2];
+    }
+}
