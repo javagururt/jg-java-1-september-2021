aee559447a3f098735b0e4efa71a7c9052f0d2bd
targutaj <39644968+targutaj@users.noreply.github.com>
2021-10-28 11:46:28 +0300

    level 5 task 21.1 - student Vadim Babcenok home

--- src/main/java/com/javaguru/student_vadim_babchenok/lesson5home/lesson3junior/task22/ForLoopDefinition6.java
+++ src/main/java/com/javaguru/student_vadim_babchenok/lesson5home/lesson3junior/task22/ForLoopDefinition6.java
@@ -1,10 +1,9 @@
-package com.javaguru.student_vadim_babchenok.lesson5home.lesson3junior.task21;
+package com.javaguru.student_vadim_babchenok.lesson5home.lesson3junior.task22;
 
-public class ForLoopDefinition5 {
+public class ForLoopDefinition6 {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
     }
-
 }
