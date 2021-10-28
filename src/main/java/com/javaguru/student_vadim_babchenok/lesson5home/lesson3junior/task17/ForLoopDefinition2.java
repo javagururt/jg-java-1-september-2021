674c5b2b9fbb3cf7605ed799baae24c19b8fceee
targutaj <39644968+targutaj@users.noreply.github.com>
2021-10-28 11:01:47 +0300

    level 5 task 17 - student Vadim Babcenok home

---
+++ src/main/java/com/javaguru/student_vadim_babchenok/lesson5home/lesson3junior/task17/ForLoopDefinition2.java
@@ -0,0 +1,11 @@
+package com.javaguru.student_vadim_babchenok.lesson5home.lesson3junior.task17;
+
+public class ForLoopDefinition2 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+//пропущена была;
