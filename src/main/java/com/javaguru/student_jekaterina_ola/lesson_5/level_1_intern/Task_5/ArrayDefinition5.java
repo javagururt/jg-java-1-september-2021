06b8ec13cf6d47fa47315220ef0e45deb949debe
kote6ka <jekaterina.ola@gmail.com>
2021-11-02 18:34:44 +0200

    lesson 5 task 5

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_5/level_1_intern/Task_5/ArrayDefinition5.java
@@ -0,0 +1,11 @@
+package com.javaguru.student_jekaterina_ola.lesson_5.level_1_intern.Task_5;
+
+//Найдите ошибку в программе и исправить её:
+
+class ArrayDefinition5 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int [] {1, 2, 3};
+    }
+
+}
