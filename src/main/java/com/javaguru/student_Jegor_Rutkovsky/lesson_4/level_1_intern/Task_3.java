e61e71c61813cea45a9c39c2c0425d9a9e3267a4
kote6ka <jekaterina.ola@gmail.com>
2021-11-02 18:22:46 +0200

    lesson 5 task 4

--- src/main/java/com/javaguru/student_Jegor_Rutkovsky/lesson_4/level_1_intern/Task_3.java
+++ src/main/java/com/javaguru/student_Jegor_Rutkovsky/lesson_4/level_1_intern/Task_3.java
@@ -1,21 +1,21 @@
-package com.javaguru.student_Jegor_Rutkovsky.lesson_4.level_1_intern;
-
-import java.util.Scanner;
-
-class Task_3 {
-    public static void main(String[] args) {
-        System.out.println("Enter number from 1 to 7");
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        switch (number) {
-            case 1 -> System.out.println("Monday");
-            case 2 -> System.out.println("Tuesday");
-            case 3 -> System.out.println("Wednesday");
-            case 4 -> System.out.println("Thursday");
-            case 5 -> System.out.println("Friday");
-            case 6 -> System.out.println("Saturday");
-            case 7 -> System.out.println("Sunday");
-            default -> System.out.println("Wrong number!!!");
-        }
-    }
-}
+//package com.javaguru.student_Jegor_Rutkovsky.lesson_4.level_1_intern;
+//
+//import java.util.Scanner;
+//
+//class Task_3 {
+//    public static void main(String[] args) {
+//        System.out.println("Enter number from 1 to 7");
+//        Scanner scanner = new Scanner(System.in);
+//        int number = scanner.nextInt();
+//        switch (number) {
+//            case 1 -> System.out.println("Monday");
+//            case 2 -> System.out.println("Tuesday");
+//            case 3 -> System.out.println("Wednesday");
+//            case 4 -> System.out.println("Thursday");
+//            case 5 -> System.out.println("Friday");
+//            case 6 -> System.out.println("Saturday");
+//            case 7 -> System.out.println("Sunday");
+//            default -> System.out.println("Wrong number!!!");
+//        }
+//    }
+//}
