a46e35fc5f61fb523b17499ad285cc649941714b
kote6ka <jekaterina.ola@gmail.com>
2021-11-02 18:15:04 +0200

    lesson 5 task 3

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_5/level_1_intern/Task_3/ArrayDefinition3.java
@@ -0,0 +1,11 @@
+package com.javaguru.student_jekaterina_ola.lesson_5.level_1_intern.Task_3;
+
+//Найдите ошибку в программе и исправить её.
+//В переменная numbers должна ссылаться на массив целых чисел.
+
+
+public class ArrayDefinition3 {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+    }
+}
