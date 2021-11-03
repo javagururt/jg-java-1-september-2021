12f741c92504f30489121feedc29e9c04c94c5ec
kote6ka <jekaterina.ola@gmail.com>
2021-11-03 18:25:52 +0200

    lesson 5 task 10

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_5/level_2_intern/Task_10/ArrayTask10.java
@@ -0,0 +1,16 @@
+package com.javaguru.student_jekaterina_ola.lesson_5.level_2_intern.Task_10;
+
+//Написать программу, в которой:
+// - создать массив целых чисел длиной 3,
+// - заполнить каждую ячейку массива любым числом,
+// - вывести значение каждой ячейки на консоль.
+
+public class ArrayTask10 {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+    }
+}
+
