0f15284a9e5d80cc8898320467b40124aef2e9b6
kote6ka <jekaterina.ola@gmail.com>
2021-11-04 19:51:19 +0200

    lesson 5 task 11

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_5/level_2_intern/Task_11/ArrayTask11.java
@@ -0,0 +1,27 @@
+package com.javaguru.student_jekaterina_ola.lesson_5.level_2_intern.Task_11;
+
+//Написать программу, в которой:
+// - заполнить каждую ячейку массива числом, запрошенным у пользователя,
+// - вывести значение каждой ячейки на консоль.
+// - создать массив целых чисел длиной 3,
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayTask11 {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        System.out.println("Please enter first number:");
+        Scanner scanner = new Scanner(System.in);
+        numbers[0] = scanner.nextInt();
+        System.out.println("Please enter second number:");
+        Scanner scanner1 = new Scanner(System.in);
+        numbers[1] = scanner.nextInt();
+        System.out.println("Please enter third number:");
+        Scanner scanner2 = new Scanner(System.in);
+        numbers[2] = scanner.nextInt();
+
+        System.out.println(Arrays.toString(numbers));
+
+    }
+}
