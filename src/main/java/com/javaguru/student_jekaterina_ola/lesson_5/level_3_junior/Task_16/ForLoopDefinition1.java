8e1325aabcf683a580c463fd9b31ce83b52eeff1
kote6ka <jekaterina.ola@gmail.com>
2021-11-14 21:40:29 +0200

    lesson 5 task 16

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_5/level_3_junior/Task_16/ForLoopDefinition1.java
@@ -0,0 +1,12 @@
+package com.javaguru.student_jekaterina_ola.lesson_5.level_3_junior.Task_16;
+
+//Найдите ошибку в программе и исправить её:
+
+class ForLoopDefinition1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
