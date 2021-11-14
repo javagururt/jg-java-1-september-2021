35031085628906b25e7b65e3edc845549294ce89
kote6ka <jekaterina.ola@gmail.com>
2021-11-14 21:55:24 +0200

    lesson 5 task 21

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_5/level_3_junior/Task_21/ForLoopDefinition5.java
@@ -0,0 +1,9 @@
+package com.javaguru.student_jekaterina_ola.lesson_5.level_3_junior.Task_21;
+
+public class ForLoopDefinition5 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++)
+            System.out.println(i);
+    }
+}
