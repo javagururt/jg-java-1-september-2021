6ea930c5525a94bb0ac0f7730f4f70f860defc39
kote6ka <jekaterina.ola@gmail.com>
2021-11-14 22:09:07 +0200

    lesson 5 task 24

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_5/level_3_junior/Task_24/orLoopDefinition8.java
@@ -0,0 +1,14 @@
+package com.javaguru.student_jekaterina_ola.lesson_5.level_3_junior.Task_24;
+
+//Исправьте код таким образом, что бы программа
+//        выводила на консоль числа от 0 до 100
+
+public class orLoopDefinition8 {
+
+    public static void main(String[] args) {
+        for (int i=0; i < 101 ; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
