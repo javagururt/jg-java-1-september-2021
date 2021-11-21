0354de51eadb7d49a2575081e19153c934936a3c
kote6ka <jekaterina.ola@gmail.com>
2021-11-21 20:08:30 +0200

    lesson 6 task 5

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_6/level_2_intern/WhileDefinition2.java
@@ -0,0 +1,13 @@
+package com.javaguru.student_jekaterina_ola.lesson_6.level_2_intern;
+
+//Найдите ошибку в программе и исправить её:
+
+public class WhileDefinition2 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
