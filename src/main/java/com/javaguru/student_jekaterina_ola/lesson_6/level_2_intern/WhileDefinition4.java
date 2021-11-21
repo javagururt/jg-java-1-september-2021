3515030142c7ce8440ddec0db8c8a0e16a4bf580
kote6ka <jekaterina.ola@gmail.com>
2021-11-21 20:11:46 +0200

    lesson 6 task 7

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_6/level_2_intern/WhileDefinition4.java
@@ -0,0 +1,14 @@
+package com.javaguru.student_jekaterina_ola.lesson_6.level_2_intern;
+
+//Найдите логическую ошибку в программе и исправить её.
+//        Программа должна выводить числа от 1 до 100 на консоль.
+
+public class WhileDefinition4 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
