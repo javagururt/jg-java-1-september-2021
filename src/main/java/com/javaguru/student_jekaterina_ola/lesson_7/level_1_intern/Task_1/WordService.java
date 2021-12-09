caa297e1885f18bfc5d82ba8a52415b730b82939
kote6ka <jekaterina.ola@gmail.com>
2021-12-09 20:22:51 +0200

    lesson 6 task 1-3

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_7/level_1_intern/Task_1/WordService.java
@@ -0,0 +1,45 @@
+package com.javaguru.student_jekaterina_ola.lesson_7.level_1_intern.Task_1;
+
+//Hаписать метод, который найдёт слово,
+//        которое в тексте встречается больше всего раз
+
+public class WordService {
+    public static void main(String[] args) {
+        WordService wordService = new WordService();
+        String sampleText = new String("Не напрасно дули ветры, Не напрасно шла гроза");
+        String word = wordService.findMostFrequentWord(sampleText);
+        System.out.println("Max wod count = " + word + " times");
+    }
+
+    public String findMostFrequentWord(String text) {
+        String addCharactersRemoval = text.trim();                  //без разделительных знаков
+        String[] newString = addCharactersRemoval.split(" ");
+        String maxWordCount = newString[0];                         //макс число встречающихся слов
+        int maxCount = 0;
+
+        for (int i = 0; i < newString.length; i++){
+            int count = wordCount (newString, newString[i]);
+            if (count > maxCount){
+                maxCount = count;
+                maxWordCount = newString[i];
+            }
+            if (count == 1){
+                System.out.println("newString [" + i + "] = '" + newString[i] + "' " + count + " times.");
+            } else {
+                System.out.println("newString [" + i + "] = '" + newString[i] + "' " + count + " times.");
+            }
+        }
+        return " '" + maxWordCount + " '" + maxCount;
+    }
+
+    public int wordCount (String[] textArray, String word){
+
+        int times  = 0;
+        for (int i = 0; i < textArray.length; i++){
+            if (textArray[i].equals(word)){
+                times++;
+            }
+        }
+        return times;
+    }
+}
---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_7/level_1_intern/Task_1/WordServiceTest.java
@@ -0,0 +1,32 @@
+package com.javaguru.student_jekaterina_ola.lesson_7.level_1_intern.Task_1;
+
+public class WordServiceTest {
+    public static void main(String[] args) {
+        WordServiceTest test = new WordServiceTest();
+        test.testFindMostFrequentWord();
+        test.testWordCount();
+
+    }
+
+    public void testFindMostFrequentWord() {
+        WordService testFindMostFrequentWordInString = new WordService();
+        String result = testFindMostFrequentWordInString.findMostFrequentWord("Не напрасно дули ветры, Не напрасно шла гроза");
+        if (result.equals("'напрасно,' 2")) {
+            System.out.println("testFindMostFrequentWord - OK");
+        } else {
+            System.out.println("testFindMostFrequentWord - FAIL");
+        }
+    }
+
+    public void testWordCount() {
+        WordService testWordCountInString = new WordService();
+        String[] testCountArray = {"Не", "напрасно", "дули", "ветры", "Не", "напрасно", "шла", "гроза"};
+        int result1 = testWordCountInString.wordCount(testCountArray, "не");
+        if (result1 == 2) {
+            System.out.println("testWordCount - OK");
+        } else {
+            System.out.println("testWordCount - FAIl");
+        }
+    }
+}
+
