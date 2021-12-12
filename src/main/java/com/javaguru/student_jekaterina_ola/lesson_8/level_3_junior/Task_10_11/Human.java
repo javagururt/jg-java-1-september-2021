57bb4551ad6afcd86c4fc79f3803ec0a022faef5
kote6ka <jekaterina.ola@gmail.com>
2021-12-12 22:07:17 +0200

    lesson 8 task 10-11

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_8/level_3_junior/Task_10_11/Human.java
@@ -0,0 +1,4 @@
+package com.javaguru.student_jekaterina_ola.lesson_8.level_3_junior.Task_10_11;
+
+public class Human {
+}
---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_8/level_3_junior/Task_10_11/LoanSystem.java
@@ -0,0 +1,9 @@
+package com.javaguru.student_jekaterina_ola.lesson_8.level_3_junior.Task_10_11;
+
+//Исправьте код программы так, что бы он компилировался.
+//        Класс LoanSystem должен остаться абстрактным.
+
+public abstract class LoanSystem {
+
+    abstract boolean canReceiveLoan (Human human);
+}
