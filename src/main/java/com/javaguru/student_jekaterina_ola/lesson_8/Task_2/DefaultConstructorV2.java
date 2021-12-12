1dcebf1fc5a5c3b6ab70b0103281ce5ea12a0a80
kote6ka <jekaterina.ola@gmail.com>
2021-12-12 17:59:17 +0200

    lesson 8 task 2

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_8/Task_2/DefaultConstructorV2.java
@@ -0,0 +1,4 @@
+package com.javaguru.student_jekaterina_ola.lesson_8.Task_2;
+
+public class DefaultConstructorV2 {
+}
---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_8/Task_2/DefaultConstructorV2Demo.java
@@ -0,0 +1,7 @@
+package com.javaguru.student_jekaterina_ola.lesson_8.Task_2;
+
+public class DefaultConstructorV2Demo {
+    public static void main (String[] args) {
+        DefaultConstructorV2 defaultConstructorV2 = new DefaultConstructorV2();
+    }
+}
