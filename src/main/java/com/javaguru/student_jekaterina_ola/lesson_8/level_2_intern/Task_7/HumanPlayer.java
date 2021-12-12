cbe063dd8d0de423baadff760ddad151c715805f
kote6ka <jekaterina.ola@gmail.com>
2021-12-12 21:31:07 +0200

    lesson 8 task 7

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_8/level_2_intern/Task_7/HumanPlayer.java
@@ -0,0 +1,5 @@
+package com.javaguru.student_jekaterina_ola.lesson_8.level_2_intern.Task_7;
+
+public class HumanPlayer extends Player {
+    HumanPlayer(String name) { super (name); }
+}
---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_8/level_2_intern/Task_7/Player.java
@@ -0,0 +1,8 @@
+package com.javaguru.student_jekaterina_ola.lesson_8.level_2_intern.Task_7;
+
+public class Player {
+
+    private String name;
+
+    Player(String name) {this.name = name; }
+}
