0abb3d9952057a1ac29b6d9280849e29002b8153
kote6ka <jekaterina.ola@gmail.com>
2021-12-12 18:11:16 +0200

    lesson 8 task 3

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_8/Task_3/DefaultConstructorV3.java
@@ -0,0 +1,15 @@
+package com.javaguru.student_jekaterina_ola.lesson_8.Task_3;
+
+// создайте get() методы для каждого свойства
+
+public class DefaultConstructorV3 {
+    private String fullName;
+    private int age;
+    private boolean male;
+    private Boolean female;
+
+    public String getFullName() { return fullName; }
+    public int getAge() { return age; }
+    public boolean isFemale() { return female; }
+    public boolean getMale() { return male; }
+}
---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_8/Task_3/DefaultConstructorV3Demo.java
@@ -0,0 +1,12 @@
+package com.javaguru.student_jekaterina_ola.lesson_8.Task_3;
+
+public class DefaultConstructorV3Demo {
+    public static void main (String[] args){
+        DefaultConstructorV3 victim = new DefaultConstructorV3();
+        System.out.println(victim.getAge());
+        System.out.println(victim.getFullName());
+        System.out.println(victim.isFemale());
+        System.out.println(victim.getMale());
+
+    }
+}
