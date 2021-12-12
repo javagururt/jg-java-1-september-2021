a50bf4f7a993cad82dcff2aeee2e6ccb38ccf27e
kote6ka <jekaterina.ola@gmail.com>
2021-12-12 17:37:09 +0200

    lesson 6 task 15

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_7/level_7_senior/UserEntity.java
@@ -0,0 +1,14 @@
+package com.javaguru.student_jekaterina_ola.lesson_7.level_7_senior;
+
+//У класса UserEntity должны быть поля:
+//        - id
+//        - имя
+//        - фамилия
+//        - персональный код
+
+public class UserEntity {
+    private int id;
+    private String name;
+    private String surname;
+    private String personalNumber;
+}
