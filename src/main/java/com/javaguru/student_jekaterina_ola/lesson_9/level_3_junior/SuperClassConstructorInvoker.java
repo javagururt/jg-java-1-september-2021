6d06787382718cb4b4d9b707974255f3fe2a6cd4
kote6ka <jekaterina.ola@gmail.com>
2021-12-19 13:59:50 +0200

    lesson 9 task 16

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_9/level_3_junior/SuperClassConstructorInvoker.java
@@ -0,0 +1,11 @@
+package com.javaguru.student_jekaterina_ola.lesson_9.level_3_junior;
+
+public class SuperClassConstructorInvoker {
+    public SuperClassConstructorInvoker() {
+        System.out.println(1);
+       // super();
+        System.out.println(2);
+    }
+}
+
+// используется для вызова суперкласса у субклассов
