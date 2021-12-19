0004397e9c2d0d6c5dd009a533897b018f440250
kote6ka <jekaterina.ola@gmail.com>
2021-12-19 14:29:17 +0200

    lesson 10 task 2

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_10/Task_2/ArrayUtil.java
@@ -0,0 +1,10 @@
+package com.javaguru.student_jekaterina_ola.lesson_10.Task_2;
+
+public interface ArrayUtil {
+    int[] createArray (int arrayLength);
+    void fillArrayWithRandomNumbers (int[] array);
+    void printArrayToConsole (int[] array);
+    int findMaxNumber (int[] array);
+    int findMinNumber (int[] array);
+
+}
