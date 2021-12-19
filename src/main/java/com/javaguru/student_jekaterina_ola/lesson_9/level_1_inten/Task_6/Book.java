2dfc9f124cf2909513fb5e5fce8ff774a5cbf126
kote6ka <jekaterina.ola@gmail.com>
2021-12-19 12:27:47 +0200

    lesson 9 task 6

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_9/level_1_inten/Task_6/Book.java
@@ -0,0 +1,10 @@
+package com.javaguru.student_jekaterina_ola.lesson_9.level_1_inten.Task_6;
+
+//Исправьте код класса Book так, чтобы он соответствовал принципу инкапсуляции.
+//        PS: сделайте так, чтобы свойства класса не были доступны из вне класса.
+
+class Book {
+
+    private String title;
+    private String author;
+}
