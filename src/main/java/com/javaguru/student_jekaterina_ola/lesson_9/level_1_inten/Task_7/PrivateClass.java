79a779ef14cb9598f24f4f489dff7088e668efd7
kote6ka <jekaterina.ola@gmail.com>
2021-12-19 12:27:59 +0200

    lesson 9 task 7

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_9/level_1_inten/Task_7/PrivateClass.java
@@ -0,0 +1,12 @@
+package com.javaguru.student_jekaterina_ola.lesson_9.level_1_inten.Task_7;
+
+//Исправьте код класса PrivateClass так, чтобы он компилировался.
+
+class PrivateClass {
+
+    private String title;
+    private String author;
+}
+
+//PS: можно ли применять идентификатор доступа private на уровне класса?
+// нет
