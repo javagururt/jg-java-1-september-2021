057c7c01528343c87aeb4f4b72a56f0573fa625d
kote6ka <jekaterina.ola@gmail.com>
2021-12-19 12:54:40 +0200

    lesson 9 task 9

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_9/level_2_intern/Task_9/PrivateConstructorВуьщ.java
@@ -0,0 +1,8 @@
+package com.javaguru.student_jekaterina_ola.lesson_9.level_2_intern.Task_9;
+
+public class PrivateConstructorВуьщ {
+   /* public static void main (String[] args){
+        PrivateConstructor privateConstructor = new PrivateConstructor();
+
+    }*/
+}
