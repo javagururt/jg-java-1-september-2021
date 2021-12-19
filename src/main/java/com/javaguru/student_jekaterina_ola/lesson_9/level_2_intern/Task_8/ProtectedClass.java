62007e0338b84b6697435c202ebc2ba697e79023
kote6ka <jekaterina.ola@gmail.com>
2021-12-19 12:40:47 +0200

    lesson 9 task 8

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_9/level_2_intern/Task_8/ProtectedClass.java
@@ -0,0 +1,12 @@
+package com.javaguru.student_jekaterina_ola.lesson_9.level_2_intern.Task_8;
+
+//Как вы думаете, почему protected классы создавать нельзя?
+//        Напиши свой ответ в коментарии над исправленным кодом.
+
+// protected class не имеет смысла, так как не будет взаимодействия
+
+public class ProtectedClass {
+
+    private String title;
+    private String author;
+}
