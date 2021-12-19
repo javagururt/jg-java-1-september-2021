5d80693c919a6a714c266a711ecf6f17816c93c8
kote6ka <jekaterina.ola@gmail.com>
2021-12-19 15:14:09 +0200

    lesson 10 task 3

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_10/Task_3/MyList.java
@@ -0,0 +1,12 @@
+package com.javaguru.student_jekaterina_ola.lesson_10.Task_3;
+
+public class MyList {
+
+    void add (String element){
+        //добавляет элемент в конец списка
+    }
+
+    void remove (int index){
+        //возвращает и удаляет последний элемент из списка под идексом
+    }
+}
