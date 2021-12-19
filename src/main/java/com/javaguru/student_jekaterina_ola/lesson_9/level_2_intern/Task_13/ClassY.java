6dfc986a86c91770485731f52b655302669f505d
kote6ka <jekaterina.ola@gmail.com>
2021-12-19 13:54:30 +0200

    lesson 9 task 13

---
+++ src/main/java/com/javaguru/student_jekaterina_ola/lesson_9/level_2_intern/Task_13/ClassY.java
@@ -0,0 +1,8 @@
+package com.javaguru.student_jekaterina_ola.lesson_9.level_2_intern.Task_13;
+
+class ClassY extends ClassX {
+        public ClassY(int i) {
+            super (i);
+            System.out.println(2);
+        }
+    }
